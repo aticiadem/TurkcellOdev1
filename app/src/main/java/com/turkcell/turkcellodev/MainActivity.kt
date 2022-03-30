4b724b6172171f871979f504e569a77c3021d63b
Adem <adematicis41@gmail.com>
2022-03-30 13:57:39 +0300

    Add camera permission functions

--- app/src/main/java/com/turkcell/turkcellodev/MainActivity.kt
+++ app/src/main/java/com/turkcell/turkcellodev/MainActivity.kt
@@ -1,23 +1,41 @@
 package com.turkcell.turkcellodev
 
-import android.app.AlertDialog
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.WindowManager
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.turkcell.turkcellodev.databinding.ActivityMainBinding
 import com.turkcell.turkcellodev.databinding.SelectImageLocationPopupBinding
+import java.io.File
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val REQ_CODE_CAMERA = 0
+    private lateinit var imagePath: String
+    private lateinit var imageUri: Uri
+    private lateinit var imageList: ArrayList<Uri>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        imageList = ArrayList()
 
         binding.buttonAddImage.setOnClickListener {
             showSelectImagePopup()
@@ -39,7 +57,8 @@ class MainActivity : AppCompatActivity() {
         )
 
         binding.buttonCamera.setOnClickListener {
-
+            checkCameraPermission()
+            dialog.dismiss()
         }
 
         binding.buttonGallery.setOnClickListener {
@@ -47,6 +66,116 @@ class MainActivity : AppCompatActivity() {
         }
 
         dialog.show()
+    }
+
+    private fun checkCameraPermission() {
+        val requestList = ArrayList<String>()
+        var permissionState = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        if (!permissionState) {
+            requestList.add(Manifest.permission.CAMERA)
+        }
+
+        permissionState = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        if (!permissionState) {
+            requestList.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        permissionState = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        if (!permissionState) {
+            requestList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (requestList.size == 0) {
+            // izin var
+            openCamera()
+        } else {
+            requestPermissions(requestList.toTypedArray(), REQ_CODE_CAMERA)
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val file = createImageFile()
+        imageUri = FileProvider.getUriForFile(this, packageName, file)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraResultLauncher.launch(intent)
+    }
+
+    private var cameraResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                imageList.add(imageUri)
+            }
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        var isItApproved = true
+        for (gr in grantResults) {
+            if (gr != PackageManager.PERMISSION_GRANTED) {
+                isItApproved = false
+                break
+            }
+        }
+
+        if (!isItApproved) {
+            var dontShowAgain = false
+            for (permission in permissions) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                    // reddedilmistir -> Sadece reddet denmistir tamamen reddet degil
+                } else if (ContextCompat.checkSelfPermission(this,
+                        permission) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // onaylandi
+                } else {
+                    // Tekrar gosterme demek
+                    dontShowAgain = true
+                    break
+                }
+            }
+
+            if (dontShowAgain) {
+                val adb = AlertDialog.Builder(this)
+                adb.setTitle(getString(R.string.need_permission))
+                    .setMessage(getString(R.string.go_settings_and_allow_permission))
+                    .setPositiveButton(getString(R.string.settings)) { _, _ ->
+                        openSettings()
+                    }
+                    .setNegativeButton(getString(R.string.cancel), null)
+                    .show()
+            }
+
+        } else {
+            when (requestCode) {
+                REQ_CODE_CAMERA -> {
+                    openCamera()
+                }
+            }
+        }
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    @Throws(IOException::class)
+    fun createImageFile(): File {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return File.createTempFile("resim", ".jpg", dir).apply {
+            imagePath = absolutePath
+        }
     }
 
 }
