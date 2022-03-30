7f06d2f2210df16249f0ff13ebec3dceeea96658
Adem <adematicis41@gmail.com>
2022-03-30 13:33:40 +0300

    Create image location popup

--- app/src/main/java/com/turkcell/turkcellodev/MainActivity.kt
+++ app/src/main/java/com/turkcell/turkcellodev/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.turkcell.turkcellodev
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.turkcell.turkcellodev.databinding.ActivityMainBinding
+import com.turkcell.turkcellodev.databinding.SelectImageLocationPopupBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +19,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.buttonAddImage.setOnClickListener {
+            showSelectImagePopup()
+        }
     }
+
+    private fun showSelectImagePopup() {
+        val dialog = Dialog(this)
+        val binding = SelectImageLocationPopupBinding.inflate(
+            LayoutInflater.from(this)
+        )
+
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(true)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT
+        )
+
+        binding.buttonCamera.setOnClickListener {
+
+        }
+
+        binding.buttonGallery.setOnClickListener {
+
+        }
+
+        dialog.show()
+    }
+
 }
